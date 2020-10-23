bae8f11c8174245ca3e0569ddbdb35ce31c26e1e
colrobin6 <70781568+colrobin6@users.noreply.github.com>
2020-10-23 18:03:07 -0500

    Create InfoController.java

---
+++ src/main/java/com/devops/calculadoraAPI/controlador/InfoController.java
@@ -0,0 +1,7 @@
+package com.devops.calculadoraAPI.controlador;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+@RestController
