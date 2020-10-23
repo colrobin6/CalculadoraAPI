1bbf43405df4a92e94da7ebde4d74b1c8d93d040
colrobin6 <70781568+colrobin6@users.noreply.github.com>
2020-10-23 18:03:47 -0500

    Update InfoController.java

--- src/main/java/com/devops/calculadoraAPI/controlador/InfoController.java
+++ src/main/java/com/devops/calculadoraAPI/controlador/InfoController.java
@@ -5,3 +5,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.net.InetAddress;
 @RestController
+@RequestMapping("/")
+public class InfoController {
+ @RequestMapping(value="/ip",method = RequestMethod.GET)
+ public ResponseEntity<String> ip(){
+ try {
+ InetAddress address = InetAddress.getLocalHost();
+ return ResponseEntity.status(200).body("<html><body><title>"+a
+ddress.getHostAddress()+"</title> <h1>"+ address.getHostAddress() +"</h1>
+<br/> <h2>" + address.getHostName() +"</h2></body></html> ");
+ } catch (Exception e) {
+ e.printStackTrace();
+ return ResponseEntity.status(500).body("Error al intentar obet
+ner la direccion ip");
+ }
+ }
+}
